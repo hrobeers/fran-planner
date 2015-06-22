94e1f9ee6b6a5da8c78367f3ac1fcff1abf52e53
hrobeers <hansrobeers@gmail.com>
2015-06-22 18:12:26 +0200

    Release build issues fixed

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -5,6 +5,6 @@ defmodule FranAppBackend.PageController do
 
   def index(conn, _params) do
     conn |> put_resp_content_type("text/html")
-         |> send_file(200, "priv/static/index.html")
+         |> send_file(200, Application.app_dir(:fran_app_backend, "priv/static/index.html"))
   end
 end
