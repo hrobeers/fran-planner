f58811f85785dc6076251d66cdb6239c22b31cd3
hrobeers <hansrobeers@gmail.com>
2015-07-17 21:37:47 +0200

    config

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :fran_app_backend, FranAppBackend.Endpoint,
   http: [port: 4321],
-  url: [host: "hrobeers.be/fran"],
+  url: [host: "localhost"],
   cache_static_manifest: "priv/static/manifest.json",
   server: true
 
